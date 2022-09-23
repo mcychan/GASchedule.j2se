a316c26c05c52513b5586dd66c3764d450d691e8
Miller Cy Chan <miller.chan@gmail.com>
2022-09-23 13:50:28 +0800

    Update Hgasso.java

--- src/hk/edu/gaSchedule/algorithm/Hgasso.java
+++ src/hk/edu/gaSchedule/algorithm/Hgasso.java
@@ -78,8 +78,6 @@ public class Hgasso<T extends Chromosome<T> > extends NsgaII<T>
 				population.get(i).updatePositions(_current_position[i]);			
 				fitness = population.get(i).getFitness();
 			}
-			else
-				population.get(i).extractPositions(_current_position[i]);
 				
 			if(fitness > _sBestScore[i]) {
 				_sBestScore[i] = fitness;
